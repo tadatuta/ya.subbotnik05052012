c1e781317a0f1eecae0ec2c6655fa1821a6506a2
Vladimir Grinenko <i@tadatuta.com>
2012-05-05 02:33:19 +0300

    carousel

---
+++ blocks/carousel/carousel.js
@@ -0,0 +1,27 @@
+BEM.DOM.decl('carousel',
+{
+    onSetMod: {
+        'js': function() {
+            this.__base();
+            this._getImages(this.cycle);
+        },
+    },
+    _getImages: function(callback) {
+        var _this = this;
+        $.ajax({
+            url: 'http://api-fotki.yandex.ru/api/recent/?format=json&limit=10',
+            dataType: 'jsonp',
+            success: function(data) {
+                _this.elem('inner').html(
+                    (data.entries.map(function(photo, idx) {
+                        return '<div class="carousel__item">' +
+                                   '<img src="' + photo.img.L.href + '">' +
+                                '</div>';
+                    })).join('')
+                );
+                callback(_this);
+            }
+        });   
+    }
+}
+);
--- pages/index/index.bemjson.js
+++ pages/index/index.bemjson.js
@@ -9,55 +9,20 @@
     ],
     content: [
         {
-            block: 'button',
-            url: '',
-            content : 'Link'
-        },
-        {
-            block: 'button',
-            type: 'submit',
-            content : 'Submit'
-        },
-        {
-            block: 'button',
-            mods: { theme: 'primary' },
-            content : 'Primary'
-        },
-        {
-            block: 'button',
-            mods: { theme: 'info' },
-            content : 'Info'
-        },
-        {
-            block: 'button',
-            mods: { theme: 'success' },
-            content : 'Success'
-        },
-        {
-            block: 'button',
-            mods: { theme: 'warning' },
-            content : 'Warning'
-        },
-        {
-            block: 'button',
-            mods: { theme: 'danger' },
-            content : 'Danger'
-        },
-        {
-            block: 'button',
-            mods: { theme: 'inverse' },
-            content : 'Inverse'
-        },
-        {
-            block: 'alert',
-            mods: { type: 'block', visibility: 'hidden' },
-            content: [
+            block: 'carousel',
+            content : [
                 {
-                    elem: 'heading',
-                    content: 'ололо'
+                    elem: 'item',
+                    content: 'bla1'
                 },
-                'опа-па!'
+                {
+                    elem: 'item',
+                    content: 'bla2'    
+                }
             ]
+        },
+        {
+            block: 'button'    
         }
     ]
 })
