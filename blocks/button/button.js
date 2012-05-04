596862b76b2ed8a99c5b632c4ca2862766860c3f
Vladimir Grinenko <i@tadatuta.com>
2012-05-05 02:56:46 +0300

    next

--- blocks/button/button.js
+++
@@ -1,9 +0,0 @@
-BEM.DOM.decl('button', {
-    onSetMod: {
-        'js': function() {
-            this.bindTo('click', function(){
-                console.log(this.getMod('theme'));
-            });
-        }    
-    }    
-});
