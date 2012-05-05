de7cf382c6eb88d9650a957a9013a9e4156b3e0f
Vladimir Grinenko <i@tadatuta.com>
2012-05-05 12:09:51 +0300

    .bem

---
+++ pages/.bem/level.js
@@ -0,0 +1,18 @@
+var extend = require('bem/lib/util').extend;
+
+exports.getTechs = function() {
+    return {
+        'bemjson.js': ''
+    };
+};
+
+exports.getConfig = function() {
+    return extend({}, this.__base() || {}, {
+        bundleBuildLevels: [
+            'bem-bl/blocks-common',
+            'bem-bl/blocks-desktop',
+            'bootstrap-bl/blocks',
+            'blocks'
+        ]
+    });
+};
